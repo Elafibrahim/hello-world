6dee8ba3f7d7ee90548ebe1aa55405f9e54e3485
Elafibrahim <95371833+Elafibrahim@users.noreply.github.com>
2021-12-01 20:09:57 +0300

    i entered two files
    How to learn upload the files

---
+++ A.java
@@ -0,0 +1,21 @@
+public class A
+{
+public static void main(String[]a)
+{
+String s1="Hello"+12;
+String s2="world";
+System.out.println(s1+s2);
+String s3="hello world";
+String s4=s3.substring(0,3);
+System.out.println(s3.substring(0,3));
+String s="hello world";
+System.out.println(s.length());
+String s6="hello";
+char ch=s.charAt(0);
+System.out.println(ch);
+boolean test= s1.equals(s2);
+System.out.println(test);
+boolean test1=s1.equalsIgnoreCase(s2);
+System.out.println(test1);
+}}
+
---
+++ elaf.java
@@ -0,0 +1,7 @@
+public class elaf
+{
+public static void main (String[]a)
+{
+System.out.println ("hellowworld");
+}
+}
